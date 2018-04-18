d324734307d9d2ac32e52ed22d4c592b3b39a74b
s185062 <mdkmsk20@yahoo.co.jp>
2018-04-18 12:18:12 +0900

    メッシュの描画を追加した

--- app.cpp
+++ app.cpp
@@ -4,33 +4,62 @@
 // ビデオキャプチャ
 #include "Capture.h"
 
+// メッシュの高さと幅
+constexpr int slices(1), stacks(1);
+
 //
 // アプリケーションの実行
 //
 void GgApplication::run()
 {
 	// ウィンドウを作成する
-	Window window("Capture");
+	Window window("Capture", 1280, 720);
 
-	// ビデオ入力
-	Capture video0(0), video1(1);
+	// 背景描画用のシェーダプログラムを読み込む
+	const GLuint shader(ggLoadShader("mesh.vert", "mesh.frag"));
+	if (!shader) throw "Can't create program object.";
+
+	// uniform 変数の場所を指定する
+	const GLuint spacingLoc(glGetUniformLocation(shader, "spacing"));
+	const GLuint imageLoc(glGetUniformLocation(shader, "image"));
+
+	// テクスチャをマッピングするメッシュを作成する
+	//   頂点座標値を vertex shader で生成するので VBO は必要ない
+	const GLuint mesh([]() { GLuint mesh; glGenVertexArrays(1, &mesh); return mesh; } ());
 
 	// 背景色を指定する
 	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
+	// 背面を描画しない
+	glEnable(GL_CULL_FACE);
+
+	// ビデオ入力
+	Capture video0(0), video1(1);
+
 	// ウィンドウが開いている間繰り返す
 	while (window.shouldClose() == GL_FALSE)
 	{
-		// ビデオをキャプチャする
-		video0.capture();
-		video1.capture();
-
 		// ウィンドウを消去する
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//
-		// ここで OpenGL による描画を行う
-		//
+		// シェーダプログラムの使用を開始する
+		glUseProgram(shader);
+
+		// メッシュの格子間隔を指定する
+		glUniform2f(spacingLoc, 1.0f / slices, 1.0f / stacks);
+
+		// テクスチャユニットを指定する
+		glUniform1i(imageLoc, 0);
+
+		// マッピングに使うテクスチャユニット
+		glActiveTexture(GL_TEXTURE0);
+
+		// ビデオをキャプチャしてテクスチャに入れる
+		video0.capture();
+
+		// メッシュを描画する
+		glBindVertexArray(mesh);
+		glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, (slices + 1) * 2, stacks);
 
 		// カラーバッファを入れ替えてイベントを取り出す
 		window.swapBuffers();
---
+++ mesh.vert
@@ -0,0 +1,35 @@
+#version 150 core
+
+//
+// 視線の回転を行う
+//
+
+// テクスチャ
+uniform sampler2D image;
+
+// テクスチャのサイズ
+vec2 size = vec2(textureSize(image, 0));
+
+// 格子間隔（格子点数の逆数）
+uniform vec2 spacing;
+
+// テクスチャ座標
+out vec2 texcoord;
+
+void main(void)
+{
+	// 頂点位置
+	//   各頂点において gl_VertexID が 0, 1, 2, 3, ... のように割り当てられるから、
+	//     x = gl_VertexID >> 1      = 0, 0, 1, 1, 2, 2, 3, 3, ...
+	//     y = 1 - (gl_VertexID & 1) = 1, 0, 1, 0, 1, 0, 1, 0, ...
+	//   のように GL_TRIANGLE_STRIP 向けの頂点座標値が得られる。
+	//   y に gl_InstaceID を足せば glDrawArrayInstanced() のインスタンスごとに y が変化する。
+	//   vec2(x, y) に格子間隔（格子点数 - 1 の逆数）spacing をかけた p は [0, 1] になる。
+	vec2 p = vec2(gl_VertexID >> 1, gl_InstanceID + 1 - (gl_VertexID & 1)) * spacing;
+
+	// 各画素の中央におけるテクスチャ座標
+	texcoord = (size * p - p + 0.5) / size;
+
+	// メッシュの頂点座標
+	gl_Position = vec4(p * 2.0 - 1.0, 0.0, 1.0);
+}
